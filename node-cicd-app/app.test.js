0ba2510bdcf43bac64c3a7b93f60b049310f4cbd
andersoncrs5 <anderson.c.rms2005@gmail.com>
2026-03-08 13:00:59 -0300

    more tests

--- node-cicd-app/app.test.js
+++ node-cicd-app/app.test.js
@@ -10,5 +10,11 @@ describe('Teste de API de Usuários', () => {
   it('Deve retornar status 200 ao buscar usuários', async () => {
     const res = await request(app).get('/users');
     expect(res.statusCode).toEqual(200);
-  });
+  }, 15000);
+
+  it('Deve retornar status 200 ao buscar post', async () => {
+    const res = await request(app).get('/posts');
+    expect(res.statusCode).toEqual(200);
+  })
+
 });
