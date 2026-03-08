e6f59f6b268263646a6dada8ea90848cc7013ada
andersoncrs5 <anderson.c.rms2005@gmail.com>
2026-03-08 13:04:18 -0300

    a

--- node-cicd-app/app.test.js
+++ node-cicd-app/app.test.js
@@ -3,9 +3,9 @@ const app = require('./app');
 const mongoose = require('mongoose');
 
 describe('Teste de API de Usuários', () => {
-  afterAll(async () => {
-    await mongoose.connection.close();
-  });
+  // afterAll(async () => {
+  //   await mongoose.connection.close();
+  // });
 
   // it('Deve retornar status 200 ao buscar usuários', async () => {
   //   const res = await request(app).get('/users');
